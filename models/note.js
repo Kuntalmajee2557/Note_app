771e86ee3e7e066a57c5a01f448d9c30b30ac8d1
gitcoder <kuntalmajee338@gmail.com>
2024-04-19 21:40:32 +0530

    model created

---
+++ models/note.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+// Define the Note schema
+const noteSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+});
+
+
+
+// Compile models from the schemas
+const Note = mongoose.model('Note', noteSchema);
+
+module.exports = Note;
