3301ba7cbc5df16b290bc2299efcf3b37a9dbfaa
gitcoder <kuntalmajee338@gmail.com>
2024-04-20 07:51:06 +0530

    password salt added

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -11,6 +12,36 @@ const userSchema = new mongoose.Schema({
       required: true
     }
   });
+
+  userSchema.pre('save', async function(next){
+    const user = this;
+
+    // Hash the password only if it has been modified (or is new)
+    if(!user.isModified('password')) return next();
+    try{
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        
+        // Override the plain password with the hashed one
+        user.password = hashedPassword;
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+  try{
+      // Use bcrypt to compare the provided password with the hashed password
+      const isMatch = await bcrypt.compare(candidatePassword, this.password);
+      return isMatch;
+  }catch(err){
+      throw err;
+  }
+}
 
   const User = mongoose.model("User", userSchema);
   module.exports = User;
